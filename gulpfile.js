293eab8a10d64d61f4acb13247256683dc472697
Killian Mazibuko <killian.mazibuko@gmail.com>
2017-05-23 18:53:50 +0200

    Added files for Week 3

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
@@ -45,7 +45,6 @@ gulp.task('imagemin', function() {
   return del(['dist/images']), gulp.src('app/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/images'))
-    .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('copyfonts', ['clean'], function() {
@@ -69,14 +68,13 @@ gulp.task('browser-sync', ['default'], function () {
       'app/**/*.html',
       'app/styles/**/*.css',
       'app/images/**/*.png',
-      'app/scripts/**/*.js',
-      'dist/**/*'
+      'app/scripts/**/*.js'
    ];
 
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "index.html"
       }
    });
         // Watch any files in dist/, reload on change
